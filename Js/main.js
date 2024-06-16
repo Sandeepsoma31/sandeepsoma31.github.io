06ec3aa06670fd17ae9e1b5e7a908a3438ed6702
Soma Sandeep <sandeepsoma031@gmail.com>
2024-06-16 21:36:30 +0530

    nav tab and scroll changes fixed

--- Js/main.js
+++ Js/main.js
@@ -38,3 +38,52 @@ window.onscroll = () =>{
 };
 
 
+//Switching between education and experience tabs
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-content');
+
+function openRelatedContent(tabName){
+        for(tablink of tablinks)
+        {
+            tablink.classList.remove('active-link');
+        };
+        for(tabcontent of tabcontents)
+        {
+            tabcontent.classList.remove('active-tab');
+        };
+        event.currentTarget.classList.add('active-link');
+        document.getElementById(tabName).classList.add('active-tab');
+}
+
+//Selecting active Navbar tab
+const navalllinks = document.querySelectorAll('.navlink');
+
+navalllinks.forEach(currentlink =>{
+    currentlink.addEventListener('click',() =>{
+        document.querySelector('.active-navlink').classList.remove('active-navlink');
+        currentlink.classList.add('active-navlink');
+    })
+})
+
+const sectionalllinks = document.querySelectorAll('.section');
+
+let currentsection = 'home';
+window.addEventListener('scroll',() =>{
+
+    sectionalllinks.forEach(presentsection => {
+        if(window.scrollY >= presentsection.offsetTop - presentsection.clientHeight/2){
+            currentsection = presentsection.id;
+        }
+    })
+
+    navalllinks.forEach(currentLink =>{
+        if(currentLink.href.includes(currentsection))
+        {
+            document.querySelector('.active-navlink').classList.remove('active-navlink'); //removes previous active-navlink
+            currentLink.classList.add('active-navlink'); //adds active-navlink
+        }
+    })
+
+})
+
+
